57ce219ae280c9edad16ad1c92e0947041b0d6f2
ilyas mohammed abid <9712+iabid@noreply.learn.zone01oujda.ma>
2026-06-25 12:09:14 +0000

    hi

--- speed_transformation/target/debug/deps/speed_transformation-5bafc6519a667f53.d
+++
@@ -1,5 +0,0 @@
-/home/iabid/Desktop/pool/piscine-rust/speed_transformation/target/debug/deps/speed_transformation-5bafc6519a667f53: src/main.rs
-
-/home/iabid/Desktop/pool/piscine-rust/speed_transformation/target/debug/deps/speed_transformation-5bafc6519a667f53.d: src/main.rs
-
-src/main.rs:
--- speed_transformation/target/debug/libspeed_transformation.d
+++
@@ -1,1 +0,0 @@
-/home/iabid/Desktop/pool/piscine-rust/speed_transformation/target/debug/libspeed_transformation.rlib: /home/iabid/Desktop/pool/piscine-rust/speed_transformation/src/lib.rs
