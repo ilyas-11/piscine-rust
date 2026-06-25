463d0f0edede6470f4cc1bce2ceb7d3b3d0691e8
ilyas-11 <ilyassabid11@gmail.com>
2026-06-25 12:16:09 +0100

    scalar

---
+++ scalar/src/lib.rs
@@ -0,0 +1,15 @@
+pub fn sum(x: u8, y: u8) -> u8 {
+   x+y
+}
+pub fn diff(x:i16,y:i16) -> i16 {
+    x-y
+}
+pub fn pro(x:i8 ,y:i8) -> i8{ 
+   x*y
+}
+pub fn quo(x:f32 , y:f32) -> f32{
+   x/y
+}
+pub fn rem(x:f32 ,y:f32) -> f32{
+   x%y
+}
