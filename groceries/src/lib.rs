a081ed54b52ff69b9623b9e5c262b9fc8abc3b1b
ilyas-11 <ilyassabid11@gmail.com>
2026-06-25 15:26:48 +0100

    groceries

---
+++ groceries/src/lib.rs
@@ -0,0 +1,8 @@
+pub fn insert(vec: &mut Vec<String>, val: String) {
+    vec.push(val);
+}
+
+pub fn at_index(slice: &[String], index: usize) -> &str {
+    let value:&str = &slice[index];
+    return value
+}
