d770062e39c45db7169d67fd0fbbd532b8d4f12e
ilyas-11 <ilyassabid11@gmail.com>
2026-06-25 18:04:35 +0100

    fibonacci2

---
+++ fibonacci2/src/lib.rs
@@ -0,0 +1,11 @@
+pub fn fibonacci(num: u32) -> u32 {
+    let mut res:u32=1;
+    let mut x:u32= 0;
+    for _ in 1..num{
+        let y= res+x;
+        x = res ;
+        res =y;
+    } 
+    res
+    
+}
