edd3adac7a18097d88aae35114dbf46e88c431c4
ilyas mohammed abid <9712+iabid@noreply.learn.zone01oujda.ma>
2026-06-26 09:55:41 +0000

    hi

--- borrow/target/debug/deps/borrow-7e9718d84a3f5f10.d
+++
@@ -1,5 +0,0 @@
-/home/iabid/Desktop/piscine-rust/borrow/target/debug/deps/libborrow-7e9718d84a3f5f10.rmeta: src/main.rs
-
-/home/iabid/Desktop/piscine-rust/borrow/target/debug/deps/borrow-7e9718d84a3f5f10.d: src/main.rs
-
-src/main.rs:
--- borrow/target/debug/deps/borrow-fc7d06b6e925d3e5.d
+++
@@ -1,5 +0,0 @@
-/home/iabid/Desktop/piscine-rust/borrow/target/debug/deps/libborrow-fc7d06b6e925d3e5.rmeta: src/lib.rs
-
-/home/iabid/Desktop/piscine-rust/borrow/target/debug/deps/borrow-fc7d06b6e925d3e5.d: src/lib.rs
-
-src/lib.rs:
