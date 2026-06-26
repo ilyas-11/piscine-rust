2cf63c6702140678f576773865d3296c823bb728
ilyas mohammed abid <9712+iabid@noreply.learn.zone01oujda.ma>
2026-06-26 10:22:46 +0000

    target

--- tuples_refs/target/debug/deps/tuples_refs-62fd9287941865f8.d
+++
@@ -1,5 +0,0 @@
-/home/iabid/Desktop/piscine-rust/tuples_refs/target/debug/deps/libtuples_refs-62fd9287941865f8.rmeta: src/lib.rs
-
-/home/iabid/Desktop/piscine-rust/tuples_refs/target/debug/deps/tuples_refs-62fd9287941865f8.d: src/lib.rs
-
-src/lib.rs:
--- tuples_refs/target/debug/deps/tuples_refs-c28fa29414f5a37f.d
+++
@@ -1,5 +0,0 @@
-/home/iabid/Desktop/piscine-rust/tuples_refs/target/debug/deps/libtuples_refs-c28fa29414f5a37f.rmeta: src/lib.rs
-
-/home/iabid/Desktop/piscine-rust/tuples_refs/target/debug/deps/tuples_refs-c28fa29414f5a37f.d: src/lib.rs
-
-src/lib.rs:
