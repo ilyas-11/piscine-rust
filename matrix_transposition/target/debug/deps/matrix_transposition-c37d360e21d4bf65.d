42dee32600810edac88305f90d2bb436872d63a5
ilyas mohammed abid <9712+iabid@noreply.learn.zone01oujda.ma>
2026-06-26 10:21:50 +0000

    target

--- matrix_transposition/target/debug/deps/matrix_transposition-c37d360e21d4bf65.d
+++
@@ -1,5 +0,0 @@
-/home/iabid/Desktop/piscine-rust/matrix_transposition/target/debug/deps/libmatrix_transposition-c37d360e21d4bf65.rmeta: src/lib.rs
-
-/home/iabid/Desktop/piscine-rust/matrix_transposition/target/debug/deps/matrix_transposition-c37d360e21d4bf65.d: src/lib.rs
-
-src/lib.rs:
