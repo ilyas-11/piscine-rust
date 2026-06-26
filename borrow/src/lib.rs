e5f48d2a6f1f735e4978cb58cd598e2c56732676
ilyas-11 <ilyassabid11@gmail.com>
2026-06-26 10:47:40 +0100

    borrow

---
+++ borrow/src/lib.rs
@@ -0,0 +1,4 @@
+pub fn str_len(s: ) -> usize {
+    s.chars().count()
+    
+}
---
+++ borrow/target/debug/deps/borrow-d7851419fda21d5e.d
@@ -0,0 +1,7 @@
+/home/iabid/Desktop/piscine-rust/borrow/target/debug/deps/libborrow-d7851419fda21d5e.rmeta: src/lib.rs
+
+/home/iabid/Desktop/piscine-rust/borrow/target/debug/deps/libborrow-d7851419fda21d5e.rlib: src/lib.rs
+
+/home/iabid/Desktop/piscine-rust/borrow/target/debug/deps/borrow-d7851419fda21d5e.d: src/lib.rs
+
+src/lib.rs:
