c316ae8a1b56c92e36c2c10c49c650b7166659bc
ilyas mohammed abid <9712+iabid@noreply.learn.zone01oujda.ma>
2026-06-26 10:21:21 +0000

    target

--- groceries/target/debug/deps/groceries-2d6182c13f37c728.d
+++
@@ -1,5 +0,0 @@
-/home/iabid/Desktop/piscine-rust/groceries/target/debug/deps/libgroceries-2d6182c13f37c728.rmeta: src/main.rs
-
-/home/iabid/Desktop/piscine-rust/groceries/target/debug/deps/groceries-2d6182c13f37c728.d: src/main.rs
-
-src/main.rs:
--- groceries/target/debug/deps/groceries-5065429395546341.d
+++
@@ -1,5 +0,0 @@
-/home/iabid/Desktop/piscine-rust/groceries/target/debug/deps/libgroceries-5065429395546341.rmeta: src/lib.rs
-
-/home/iabid/Desktop/piscine-rust/groceries/target/debug/deps/groceries-5065429395546341.d: src/lib.rs
-
-src/lib.rs:
