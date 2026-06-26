b2b6d36994e1de3580981dddbd4182710b1197a3
ilyas mohammed abid <9712+iabid@noreply.learn.zone01oujda.ma>
2026-06-26 10:21:41 +0000

    target

--- looping/target/debug/deps/looping-446b882fc461794f.d
+++
@@ -1,5 +0,0 @@
-/home/iabid/Desktop/piscine-rust/looping/target/debug/deps/liblooping-446b882fc461794f.rmeta: src/main.rs
-
-/home/iabid/Desktop/piscine-rust/looping/target/debug/deps/looping-446b882fc461794f.d: src/main.rs
-
-src/main.rs:
--- looping/target/debug/deps/looping-99a0aa7cc48253e0.d
+++
@@ -1,5 +0,0 @@
-/home/iabid/Desktop/piscine-rust/looping/target/debug/deps/liblooping-99a0aa7cc48253e0.rmeta: src/main.rs
-
-/home/iabid/Desktop/piscine-rust/looping/target/debug/deps/looping-99a0aa7cc48253e0.d: src/main.rs
-
-src/main.rs:
