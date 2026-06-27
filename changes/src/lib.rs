437fc1ac692b7c38043b376e8a5061e4e3c2c96e
ilyas-11 <ilyassabid11@gmail.com>
2026-06-27 15:22:43 +0100

    changes

---
+++ changes/src/lib.rs
@@ -0,0 +1,24 @@
+#[derive(Debug, Eq, PartialEq, Clone)]
+pub struct Light {
+	pub alias: String,
+	pub brightness: u8,
+}
+
+impl Light {
+	pub fn new(alias: &str) -> Self {
+        Self{
+            alias:alias.to_string(),
+            brightness:0
+        }
+	}
+}
+
+pub fn change_brightness(lights: &mut [Light], alias: &str, value: u8) {
+    for Lig in lights.iter_mut(){
+        if Lig.alias==alias{
+            Lig.brightness = value;
+        }
+    }
+    
+    
+}
