0300fb4e51a5c5493ae5bcb965bfd77590675669
ilyas-11 <ilyassabid11@gmail.com>
2026-06-28 12:40:42 +0100

    capitalizing

---
+++ capitalizing/src/lib.rs
@@ -0,0 +1,23 @@
+pub fn capitalize_first(input: &str) -> String {
+    let mut ch =input.chars();
+    match ch.next(){
+        Some(f)=>{
+            f.to_uppercase().collect::<String>()+ch.as_str()
+        }
+        None =>String::new()
+    }
+}
+
+pub fn title_case(input: &str) -> String {
+    input.split(' ').map(capitalize_first).collect::<Vec<String>>().join(" ")
+}
+
+pub fn change_case(input: &str) -> String {
+    input.chars().map(|c|{
+        if c.is_uppercase(){
+            c.to_lowercase().collect::<String>()
+        }else{
+            c.to_uppercase().collect::<String>()
+        }
+    }).collect()
+}
