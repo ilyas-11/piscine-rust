109697648a7ed394160dab768e0dc6de55e378bf
ilyas-11 <ilyassabid11@gmail.com>
2026-06-28 10:47:09 +0100

    card_deck

---
+++ card_deck/src/lib.rs
@@ -0,0 +1,101 @@
+use rand::Rng;
+
+
+#[derive(Debug)]
+pub enum Suit {
+    Heart,
+    Diamond,
+    Spade,
+    Club,
+}
+
+
+#[derive(Debug)]
+pub enum Rank {
+    Ace,
+    Number(u8),
+    Queen,
+    Jack,
+    King,
+}
+
+
+impl Suit {
+
+    pub fn random() -> Suit {
+
+        let value = rand::thread_rng()
+            .gen_range(1..=4);
+
+        Suit::translate(value)
+    }
+
+
+    pub fn translate(value:u8)->Suit {
+
+        match value {
+
+            1 => Suit::Heart,
+            2 => Suit::Diamond,
+            3 => Suit::Spade,
+            _ => Suit::Club,
+
+        }
+    }
+}
+
+
+
+impl Rank {
+
+    pub fn random()->Rank {
+
+        let value = rand::thread_rng()
+            .gen_range(1..=13);
+
+        Rank::translate(value)
+    }
+
+
+    pub fn translate(value:u8)->Rank {
+
+        match value {
+
+            1 => Rank::Ace,
+
+            2..=10 => Rank::Number(value),
+
+            11 => Rank::Jack,
+
+            12 => Rank::Queen,
+
+            _ => Rank::King,
+
+        }
+    }
+}
+
+
+
+#[derive(Debug)]
+pub struct Card {
+
+    pub suit: Suit,
+
+    pub rank: Rank,
+
+}
+
+
+
+pub fn winner_card(card:&Card)->bool {
+
+    match (&card.suit,&card.rank){
+
+        (Suit::Spade, Rank::Ace)=>true,
+
+        _=>false,
+
+    }
+
+}
