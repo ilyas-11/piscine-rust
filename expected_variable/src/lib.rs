13faec1a783c74f5277f0d8e481eabc2ffbb35e7
iabid <ilyassabid11@gmail.com>
2026-07-04 16:55:47 +0100

    macro_calculator

---
+++ expected_variable/src/lib.rs
@@ -0,0 +1,18 @@
+use convert_case::{Case, Casing};
+use edit_distance as Ed;
+
+pub fn expected_variable(expected: &str, s: &str) -> Option<String> {
+    let s = &s.to_lowercase();
+    let expected = &expected.to_lowercase();
+    if !s.is_case(Case::Camel) && !s.is_case(Case::Snake) {
+        None
+    } else {
+        let edit = Ed::edit_distance(s, expected);
+        let res: i64 = 100 - ((edit as i64 * 100) / s.len() as i64);
+        if res <= 50 {
+            None
+        } else {
+            Some(res.to_string() + "%")
+        }
+    }
+}
